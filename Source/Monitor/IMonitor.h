89f3e27d95189e2f1f24667b0e9b07e958ae1b30
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2026-05-20 14:11:00 +0900

    Add: 監視用クラスを追加

---
+++ Source/Monitor/IMonitor.h
@@ -0,0 +1,36 @@
+﻿/**
+ * @file IMonitor.h
+ * @brief 監視モジュールのインターフェース
+ */
+#pragma once
+#include "stdafx.h"
+
+
+namespace app
+{
+
+
+	/**
+	 * @brief 各監視モジュール（カメラ・ネットワーク・Notion等）が実装すべきインターフェース
+	 */
+	class IMonitor
+	{
+	public:
+		/** @brief デストラクタ */
+		virtual ~IMonitor() = default;
+
+		/**
+		 * @brief 監視を1回実行し、遷移すべき状態名を返す
+		 * @return 遷移先の状態名。遷移不要の場合は空文字列を返す
+		 */
+		virtual std::string Observe() = 0;
+
+		/**
+		 * @brief 監視モジュール名を返す
+		 * @return モジュール名文字列
+		 */
+		virtual const char* GetName() const = 0;
+	};
+
+
+} // namespace app
---
+++ Source/Monitor/MonitorThread.cpp
@@ -0,0 +1,72 @@
+﻿/**
+ * @file MonitorThread.cpp
+ * @brief 監視スレッド統括クラス実装
+ */
+#include "stdafx.h"
+#include "MonitorThread.h"
+#include "IMonitor.h"
+#include "Source/StateMachine/StateMachine.h"
+
+
+namespace app
+{
+	MonitorThread::MonitorThread()
+	{}
+
+
+	MonitorThread::~MonitorThread()
+	{}
+
+
+	void MonitorThread::Init(StateMachine& stateMachine, int intervalMs)
+	{
+		m_stateMachine = &stateMachine;
+		m_interval = std::chrono::milliseconds(intervalMs);
+	}
+
+
+	void MonitorThread::AddMonitor(std::unique_ptr<IMonitor> monitor)
+	{
+		m_monitors.push_back(std::move(monitor));
+	}
+
+
+	void MonitorThread::Start()
+	{
+		if (m_isRunning) return;
+
+		m_isRunning = true;
+		m_thread = std::thread(&MonitorThread::ThreadLoop, this);
+		std::cout << "[MonitorThread] 監視スレッドを開始しました" << std::endl;
+	}
+
+
+	void MonitorThread::Stop()
+	{
+		m_isRunning = false;
+		if (m_thread.joinable())
+		{
+			m_thread.join();
+		}
+		std::cout << "[MonitorThread] 監視スレッドを停止しました" << std::endl;
+	}
+
+
+	void MonitorThread::ThreadLoop()
+	{
+		while (m_isRunning)
+		{
+			for (auto& monitor : m_monitors)
+			{
+				const std::string nextState = monitor->Observe();
+				if (!nextState.empty())
+				{
+					m_stateMachine->Transition(nextState);
+				}
+			}
+			std::this_thread::sleep_for(m_interval);
+		}
+	}
+
+
+} // namespace app
