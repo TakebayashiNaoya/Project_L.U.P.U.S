46b4440b82968e206c0c4252225417cec3cec41e
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2026-05-20 14:10:38 +0900

    Add: 音声パイプラインのクラスを追加

---
+++ Source/Audio/AudioPipeline.cpp
@@ -0,0 +1,55 @@
+﻿/**
+ * @file AudioPipeline.cpp
+ * @brief 音声パイプラインクラス実装（スケルトン）
+ */
+#include "stdafx.h"
+#include "Source/Audio/AudioPipeline.h"
+
+
+namespace app
+{
+
+
+	void AudioPipeline::Start()
+	{
+		if (m_isRunning) return;
+
+		m_isRunning = true;
+		m_thread = std::thread(&AudioPipeline::ThreadLoop, this);
+		std::cout << "[AudioPipeline] 音声パイプラインを開始しました" << std::endl;
+	}
+
+
+	void AudioPipeline::Stop()
+	{
+		m_isRunning = false;
+
+		if (m_thread.joinable())
+		{
+			m_thread.join();
+		}
+		std::cout << "[AudioPipeline] 音声パイプラインを停止しました" << std::endl;
+	}
+
+
+	std::string AudioPipeline::GetLastTranscription() const
+	{
+		std::lock_guard<std::mutex> lock(m_mutex);
+		return m_lastTranscription;
+	}
+
+
+	void AudioPipeline::ThreadLoop()
+	{
+		while (m_isRunning)
+		{
+			// TODO: フェーズ2で以下を実装する
+			//       1. Silero VAD により音声区間を検出する
+			//       2. 検出した音声区間を Whisper.cpp に渡してテキストに変換する
+			//       3. 変換結果を m_lastTranscription に格納する
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
+	}
+
+
+} // namespace app
---
+++ Source/Audio/AudioPipeline.h
@@ -0,0 +1,49 @@
+﻿/**
+ * @file AudioPipeline.h
+ * @brief 音声パイプラインクラス（スケルトン）
+ */
+#pragma once
+#include "stdafx.h"
+#include "Source/Audio/IAudioPipeline.h"
+
+
+namespace app
+{
+
+
+	/**
+	 * @brief Silero VAD + Whisper.cpp を用いた音声認識パイプライン
+	 * @details フェーズ2以降で実装する。フェーズ1ではスケルトンのみ。
+	 */
+	class AudioPipeline : public IAudioPipeline
+	{
+	public:
+		/** @brief 音声パイプラインを開始する */
+		void Start() override;
+		/** @brief 音声パイプラインを停止する */
+		void Stop() override;
+		/**
+		 * @brief 最後に認識されたテキストを返す
+		 * @return 認識テキスト文字列
+		 */
+		std::string GetLastTranscription() const override;
+
+
+	private:
+		/** @brief 音声監視スレッドのメインループ */
+		void ThreadLoop();
+
+
+	private:
+		/** 音声監視スレッド */
+		std::thread m_thread;
+		/** 音声監視スレッドの実行状態 */
+		std::atomic<bool> m_isRunning{ false };
+		/** 最後に認識されたテキストを保護するミューテックス */
+		mutable std::mutex m_mutex;
+		/** 最後に認識されたテキスト */
+		std::string m_lastTranscription;
+	};
+
+
+} // namespace app
