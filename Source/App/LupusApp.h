721133e537e75a937a0dcce2118581b45cad5c1f
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2026-05-20 14:10:18 +0900

    Add: コアになるAppを追加

---
+++ Source/App/LupusApp.h
@@ -0,0 +1,56 @@
+﻿/**
+ * @file LupusApp.h
+ * @brief アプリケーション統括クラス
+ */
+#pragma once
+#include "stdafx.h"
+
+
+namespace app
+{
+
+
+	/** 前方宣言 */
+	class ProfileLoader;
+	class StateMachine;
+	class MonitorThread;
+	class AudioPipeline;
+
+
+	/**
+	 * @brief L.U.P.U.S. の全モジュールを生成・起動・終了する統括クラス
+	 */
+	class LupusApp
+	{
+	public:
+		LupusApp();
+		~LupusApp();
+
+		/**
+		 * @brief アプリケーションを初期化する
+		 * @return 初期化に成功した場合 true
+		 */
+		bool Init();
+
+		/** @brief メインループを実行する（ブロッキング） */
+		void Run();
+
+		/** @brief アプリケーションを終了する */
+		void Shutdown();
+
+
+	private:
+		/** システムプロファイルの読み込みクラス */
+		std::unique_ptr<ProfileLoader> m_profileLoader;
+		/** ステートマシン管理クラス */
+		std::unique_ptr<StateMachine>  m_stateMachine;
+		/** 監視スレッド統括クラス */
+		std::unique_ptr<MonitorThread> m_monitorThread;
+		/** 音声パイプラインクラス */
+		std::unique_ptr<AudioPipeline> m_audioPipeline;
+		/**< アプリケーションの実行状態 */
+		std::atomic<bool> m_isRunning{ false };
+	};
+
+
+} // namespace app
